ccc2789edfc470e855f68d4e6dd7585ffc03b299
MicahJank <MicahJank@users.noreply.github.com>
2019-03-05 15:51:44 -0600

    percentage-calulator
    created functionality for the X is what % of Y calculation, as well as set up some error handling.

--- calc.js
+++ calc.js
@@ -5,6 +5,23 @@ const numField2 = document.getElementById('numField2');
 // We also need to grab and store the result that will be displayed on the screen, otherwise the user wont know what the percentage is!
 const resultField = document.getElementById('resultField');
 
+// The form variable stores the form that users use to enter in their numbers. We get the form by grabbing its Id.
+const form = document.getElementById('xIsWhatPercentOfY');
 
-// to calculate percentage first divide the the first number by the second, then multiply the result by 100
-const calculatePercentage = (num1, num2) => (num1 / num2) * 100;
+
+form.addEventListener('submit', (event) => {
+    // const calc = (numField1.value / numField2.value) * 100;
+    // resultField.value = `Result: ${calc}%`;
+
+    if (!numField1.value || !numField2.value) {
+        alert(`Please enter values in all the fields!`);
+    } else {
+        const num1 = parseFloat(numField1.value);
+        const num2 = parseFloat(numField2.value);
+        const result = (num1 / num2) * 100;
+        
+        resultField.innerText = `Result: ${result}%`;
+        event.preventDefault();
+    }
+    
+});
